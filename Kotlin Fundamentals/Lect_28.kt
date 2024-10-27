26944c3a48192f666c38765935e541b5e119d9f5
Dhairya Pandya <dhairyapandya05@gmial.com>
2024-10-27 18:11:52 +0530

    Completed teh fundamental kotlin

---
+++ Kotlin Fundamentals/Lect_28.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var name:String
+    var mybyte:Byte
+    var myShort:Short
+    mybyte=120
+    myShort=12999
+    val age=21
+    name="Dhairya"
+    print("$mybyte")
+    print("$myShort")
+}
